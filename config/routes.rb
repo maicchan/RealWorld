5f78315f97244210f5ea4d2dd05570a90567c3f2
github maicchan <pororikarori.0321@gmail.com>
2023-07-07 20:09:51 +0900

    ヘルスチェックコントローラーを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'elb/health_check' => 'elb#health_check'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
