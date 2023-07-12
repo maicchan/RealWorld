c08ab4d1467282d0e3baa3de9fdccbeb95c63861
github maicchan <pororikarori.0321@gmail.com>
2023-07-12 21:04:04 +0900

    デプロイに関わる編集

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+# port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,5 +41,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
-app_root = File.expand_path("../..", __FILE__)
-bind "unix:#{app_root}/tmp/sockets/puma.sock"
+bind "unix:///var/www/RealWorld/tmp/sockets/puma.sock"
